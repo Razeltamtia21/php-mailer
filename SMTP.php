af33dd47d3131b5504d13bf6754c21557834641a
Razel <156520794+Razeltamtia21@users.noreply.github.com>
2024-10-18 20:27:00 +0700

    Update SMTP.php

--- SMTP.php
+++ SMTP.php
@@ -4,22 +4,20 @@ namespace PHPMailer\PHPMailer;
 
 class SMTP
 {
-    // Properties and methods for SMTP connection...
 
     public function connect($host, $port, $timeout = 30)
     {
-        // Connect to the SMTP server
+        
     }
 
     public function auth($username, $password)
     {
-        // Authenticate with the SMTP server
+       
     }
 
     public function send($message)
     {
-        // Send the message
+        
     }
 
-    // Other necessary methods...
 }
